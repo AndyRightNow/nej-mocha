da70e399f0d81e1f35d5f4491eeb1819ae68674d
AndyRightNow <naruto199507@gmail.com>
2017-03-17 19:19:36 +0800

    Add config / remove unnecessary variables

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
     DEFAULT_PORT: 8004,
     TEST_INDEX: "testIndex",
-    defaultReporter: "spec",
-    phantomJSPath: "./node_modules/phantomjs-prebuilt/bin/phantomjs",
-    mochaPhantomJSCorePath: "./node_modules/mocha-phantomjs-core/mocha-phantomjs-core.js"
+    GLOBAL_PROMISE: `globalPromise`
 };
