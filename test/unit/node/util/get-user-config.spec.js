76ec6296fb10e822cad0fefbcc3e262858f94c64
AndyRightNow <naruto199507@gmail.com>
2017-04-28 19:04:24 +0800

    Improve tests

--- test/unit/node/util/get-user-config.spec.js
+++ test/unit/node/util/get-user-config.spec.js
@@ -1,13 +1,12 @@
-var expect = require('chai').expect;
-var getUserConfig = require('./../../../../src/node/util/get-user-config');
+/* global describe, it */
+
+var expect = require('chai').expect
+var getUserConfig = require('./../../../../src/node/util/get-user-config')
 
 describe('src/node/util/get-user-config', () => {
-
-    describe('If no path is given', () => {
-        it('should return an object from nej-mocha.conf.js', () => {
-            expect(getUserConfig()).to.be.a("Object");
-        });
-
-    });
-
-});
+  describe('If no path is given', () => {
+    it('should return an object from nej-mocha.conf.js', () => {
+      expect(getUserConfig()).to.be.a('Object')
+    })
+  })
+})
